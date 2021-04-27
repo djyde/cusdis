6eb4168248f0762be18e06008e239ca8307c2f0b
selimdoyranli <selim@radkod.com>
2021-04-28 02:42:22 +0300

    :globe_with_meridians: Translate to tr

---
+++ widget/lang/tr.js
@@ -0,0 +1,20 @@
+export default {
+  powered_by: 'Cusdis tarafından',
+  post_comment: 'Yorumu Gönder',
+  loading: 'Yükleniyor',
+
+  // comment
+  email: 'Email (isteğe bağlı)',
+  nickname: 'Nickname',
+  reply_placeholder: 'Yanıtla...',
+  reply_btn: 'Yanıtla',
+  sending: 'gönderiliyor...',
+
+  // reply
+  mod_badge: 'MOD',
+  content_is_required: 'Yorum gereklidir',
+  nickname_is_required: 'Nickname gereklidir',
+
+  comment_has_been_sent:
+    'Yorumunuz gönderildi. Lütfen onaylanmasını bekleyin.',
+}
