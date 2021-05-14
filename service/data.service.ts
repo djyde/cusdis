b41de29a2fccdbd4a8043d791a94aaa3b9861120
Randy <randypriv@gmail.com>
2021-05-14 20:17:14 +0800

    use dataschema #102

--- service/data.service.ts
+++ service/data.service.ts
@@ -4,10 +4,29 @@ const parser = require('xml2json')
 import TurndownService from 'turndown'
 import { statService } from './stat.service'
 const turndownService = new TurndownService()
+
+export type DataSchema = {
+  pages: Array<{
+    uniqueId: string
+    pageId: string
+    url?: string
+    title?: string
+  }>
+  comments: Array<{
+    id: string
+    content: string
+    createdAt: string
+    by_nickname: string
+    by_email?: string
+    pageUniqueId: string
+    parentId: string
+  }>
+}
+
 export class DataService {
-  async importFromDisqus(projectId: string, xmlData: string) {
+  disqusAdapter(xmlData: string): DataSchema {
     const parsed = JSON.parse(parser.toJson(xmlData)).disqus
-    const threads = parsed.thread.filter(
+    const threads = (parsed.thread.filter(
       (_) => typeof _.id === 'string' && _.isDeleted === 'false',
     ) as Array<{
       'dsq:id': string
@@ -16,8 +35,16 @@ export class DataService {
       title: string
       createdAt: string
       isDeleted: string
-    }>
-    const posts = parsed.post as Array<{
+    }>).map((_) => {
+      return {
+        uniqueId: _['dsq:id'],
+        pageId: _.id,
+        url: _.link,
+        title: _.title,
+      } as DataSchema['pages'][0]
+    })
+
+    const posts = (parsed.post as Array<{
       'dsq:id': string
       message: string
       createdAt: string
@@ -33,20 +60,42 @@ export class DataService {
       parent?: {
         'dsq:id': string
       }
-    }>
+    }>)
+      .map((_) => {
+        return {
+          pageUniqueId: _.thread['dsq:id'],
+          by_nickname: _.author.name,
+          content: turndownService.turndown(_.message),
+          id: _['dsq:id'],
+          createdAt: _.createdAt,
+          pageId: _.thread['dsq:id'],
+          parentId: _.parent?.['dsq:id'],
+        } as DataSchema['comments'][0]
+      })
+      .filter(
+        (post) =>
+          threads.findIndex((_) => _.uniqueId === post.pageUniqueId) !==
+          -1,
+      )
 
-    // import threads into pages
+    return {
+      pages: threads,
+      comments: posts,
+    }
+  }
+
+  async import(projectId: string, schema: DataSchema) {
     const pages = await prisma.$transaction(
-      threads.map((thread) => {
+      schema.pages.map((thread) => {
         return prisma.page.upsert({
           where: {
-            id: thread['dsq:id'],
+            id: thread.uniqueId,
           },
           create: {
-            id: thread['dsq:id'],
+            id: thread.uniqueId,
             projectId,
-            slug: thread.id,
-            url: thread.link,
+            slug: thread.pageId,
+            url: thread.url,
             title: thread.title,
           },
           update: {},
@@ -54,38 +103,36 @@ export class DataService {
       }),
     )
 
-    const filteredPosts = posts.filter((post) => {
-      // filter the posts that missing parent thread
-      return (
-        threads.findIndex((_) => _['dsq:id'] === post.thread['dsq:id']) !== -1
-      )
-    })
-
     const upsertedPosts = await prisma.$transaction(
-      filteredPosts
-        .map((post) => {
-          return prisma.comment.upsert({
-            where: {
-              id: post['dsq:id'],
-            },
-            create: {
-              id: post['dsq:id'],
-              content: turndownService.turndown(post.message),
-              createdAt: post.createdAt,
-              by_nickname: post.author.name,
-              pageId: post.thread['dsq:id'],
-              parentId: post.parent?.['dsq:id'],
-            },
-            update: {},
-          })
-        }),
+      schema.comments.map((post) => {
+        return prisma.comment.upsert({
+          where: {
+            id: post.id,
+          },
+          create: {
+            id: post.id,
+            content: post.content,
+            createdAt: post.createdAt,
+            by_nickname: post.by_nickname,
+            pageId: post.pageUniqueId,
+            parentId: post.parentId,
+          },
+          update: {},
+        })
+      }),
     )
-
-    statService.capture('import_disqus')
 
     return {
       threads: pages,
-      posts: upsertedPosts,
+      posts: upsertedPosts
     }
+  }
+
+  async importFromDisqus(projectId: string, xmlData: string) {
+
+    const result = await this.import(projectId, this.disqusAdapter(xmlData))
+    statService.capture('import_disqus')
+
+    return result
   }
 }
