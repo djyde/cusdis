aa043b22d190d4c16923f2bd9c8cf11ea576ea2a
rodrigoalcarazdelaosa <rodri.alcaraz@gmail.com>
2021-05-18 23:18:59 +0200

    Create ca.js

---
+++ widget/lang/ca.js
@@ -0,0 +1,21 @@
+window.CUSDIS_LOCALE = {
+    powered_by: 'Desenvolupat per Cusdis',
+    post_comment: 'Comentar',
+    loading: 'Carregant',
+  
+    // comment
+    email: 'Correu electrònic (opcional)',
+    nickname: 'Nom',
+    reply_placeholder: 'Respondre...',
+    reply_btn: 'Respondre',
+    sending: 'Enviant...',
+  
+    // reply
+    mod_badge: 'MOD',
+    content_is_required: 'El contingut és obligatori',
+    nickname_is_required: 'El nom és obligatori',
+  
+    comment_has_been_sent:
+      'El teu comentari ha estat enviat. Si us plau, espera la seva aprovació.',
+  }
+  
