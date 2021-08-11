1ea28e779f3b04f9543240a8b4bf22d54a858fb4
Klim <59335849+paparrot@users.noreply.github.com>
2021-08-11 13:08:16 +0300

    Create ru.js

---
+++ widget/lang/ru.js
@@ -0,0 +1,20 @@
+window.CUSDIS_LOCALE = {
+  powered_by: 'Создано с Cusdis',
+  post_comment: 'Отправить',
+  loading: 'Загружаем комментарии...',
+
+  // comment
+  email: 'Email (необязательно)',
+  nickname: 'Имя',
+  reply_placeholder: 'Ваш комментарий',
+  reply_btn: 'Ответить',
+  sending: 'Отправляется...',
+
+  // reply
+  mod_badge: 'MOD',
+  content_is_required: 'Заполните текст комментария',
+  nickname_is_required: 'Заполните имя',
+
+  comment_has_been_sent:
+    'Комментарий отправлен. Он будет опубликован после модерации.',
+}
