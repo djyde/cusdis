4810449b415debd5c199ce57ec0d6518cb8d0561
Niels van Brakel <Niels.van.Brakel@conclusion.nl>
2021-12-07 12:32:03 +0100

    Added dutch langauge

---
+++ widget/lang/nl.js
@@ -0,0 +1,20 @@
+window.CUSDIS_LOCALE = {
+  powered_by: 'Reacties mogelijk gemaakt door Cusdis',
+  post_comment: 'Reageer',
+  loading: 'Laden',
+
+  // comment
+  email: 'Email (optioneel)',
+  nickname: 'Gebruikersnaam',
+  reply_placeholder: 'Reageer...',
+  reply_btn: 'Reageer',
+  sending: 'versturen...',
+
+  // reply
+  mod_badge: 'MOD',
+  content_is_required: 'Tekst is verplicht',
+  nickname_is_required: 'Gebruikersnaam is verplicht',
+
+  comment_has_been_sent:
+    'Je reactie is verstuurd. Wacht op goedkeuring',
+}
