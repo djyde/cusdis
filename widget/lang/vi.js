46859fac0a3c595057e01f3d39cd81e3d4d33928
Parker Vu <48187449+parkervu@users.noreply.github.com>
2022-05-31 20:49:08 +0700

    Create vi.js

---
+++ widget/lang/vi.js
@@ -0,0 +1,20 @@
+window.CUSDIS_LOCALE = {
+  powered_by: 'Hệ thống bình luận bởi Cusdis',
+  post_comment: 'Gửi',
+  loading: 'Đang tải',
+
+  // comment
+  email: 'Email (nếu có)',
+  nickname: 'Nickname (tên)',
+  reply_placeholder: 'Trả lời...',
+  reply_btn: 'Trả lời',
+  sending: 'Đang gửi...',
+
+  // reply
+  mod_badge: 'Quản trị viên',
+  content_is_required: 'Hãy nhập nội dung',
+  nickname_is_required: 'Hãy nhập nickname (tên)',
+
+  comment_has_been_sent:
+    'Bình luận của bạn đã được gửi đi và đang chờ được phê duyệt.',
+}
