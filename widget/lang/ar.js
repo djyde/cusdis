3cf2728a877056962d0f8845d1f0f303967829db
Amr <akkk33@protonmail.com>
2022-09-02 03:31:05 +0200

    Fixed Arabic translation

--- widget/lang/ar.js
+++ widget/lang/ar.js
@@ -1,19 +1,19 @@
 window.CUSDIS_LOCALE = {
     powered_by: 'CUSDIS نظام تعليقات مقدم من',
-    post_comment: 'تعليق',
-    loading: '... جارى التحميل',
+    post_comment: 'أرسل تعليق',
+    loading: 'جارى التحميل',
   
     // comment
     email: 'البريد الالكترونى (اختيارى)',
-    nickname: 'أسمك',
+    nickname: 'الإسم',
     reply_placeholder: '... رد',
     reply_btn: 'رد',
-    sending: '... جارى الارسال',
+    sending: '... جارِ الارسال',
   
     // reply
-    mod_badge: 'MOD',
+    mod_badge: 'مدير',
     content_is_required: 'المحتوى مطلوب',
-    nickname_is_required: 'الأسم مطلوب',
+    nickname_is_required: 'الإسم مطلوب',
   
     comment_has_been_sent:
       '.تم ارسال تعليقك بنجاح , فى انتظار الموافقه عليه',
