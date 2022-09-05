b2ea5f461530d0f8f34bd0e763113f12e3861d42
janrippl <33397305+janrippl@users.noreply.github.com>
2022-09-05 08:08:51 +0200

    Update and rename en.js to cs.js
    Czech language

---
+++ widget/lang/cs.js
@@ -0,0 +1,20 @@
+export default {
+  powered_by: 'Komentáře vytvořeny za pomoci Cusdis',
+  post_comment: 'Komentář',
+  loading: 'Načítám...',
+
+  // comment
+  email: 'E-mail (volitelné)',
+  nickname: 'Přezdívka',
+  reply_placeholder: 'Odpovědět...',
+  reply_btn: 'Odpověď',
+  sending: 'Posílám...',
+
+  // reply
+  mod_badge: 'MOD',
+  content_is_required: 'Text komentáře je povinný',
+  nickname_is_required: 'Přezdívka je povinná',
+
+  comment_has_been_sent:
+    'Váš komentář byl odeslán. Počkejte si prosím na schválení...',
+}
