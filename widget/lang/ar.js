8a94341f639a59ca6910737ad1d66595cd11f225
BodeyMessi <105023070+BodeyMessi@users.noreply.github.com>
2022-09-06 13:27:55 +0200

    Update ar.js

--- widget/lang/ar.js
+++ widget/lang/ar.js
@@ -1,19 +1,19 @@
 window.CUSDIS_LOCALE = {
     powered_by: 'CUSDIS نظام تعليقات مقدم من',
-    post_comment: 'تعليق',
+    post_comment: 'إرسال تعليق',
     loading: '... جارى التحميل',
   
     // comment
-    email: '<p style="text-align: right;">البريد (اختياري)</p>',
+    email: '<p style="text-align: right;">البريد الإلكتروني (اختياري)</p>',
     nickname: '<p style="text-align: right;">الإسم</p>',
-    reply_placeholder: '<p style="text-align: right;">أكتب تعليقك</p>',
+    reply_placeholder: '<p style="text-align: right;">أكتب تعليق ..</p>',
     reply_btn: 'رد',
-    sending: '... جارى الارسال',
+    sending: '... جار الارسال',
   
     // reply
-    mod_badge: 'MOD',
+    mod_badge: 'مدير',
     content_is_required: 'المحتوى مطلوب',
-    nickname_is_required: 'الأسم مطلوب',
+    nickname_is_required: 'الإسم مطلوب',
   
     comment_has_been_sent:
       '.تم ارسال تعليقك بنجاح , فى انتظار الموافقه عليه',
