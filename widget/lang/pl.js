0d017e77f21f478df851477b1f19af6e3c22c6b8
Ketjow3 <56757201+wtkksl@users.noreply.github.com>
2022-10-06 00:10:32 +0200

    Create pl.js
    polish language translate

---
+++ widget/lang/pl.js
@@ -0,0 +1,20 @@
+window.CUSDIS_LOCALE = {
+  powered_by: 'Komentarze tworzone przez Cusdis',
+  post_comment: 'Dodaj komentarz',
+  loading: 'Wczytywanie',
+
+  // comment
+  email: 'Email (opcjonalnie)',
+  nickname: 'Ksywka',
+  reply_placeholder: 'Odpowiedz...',
+  reply_btn: 'Odpowiedz',
+  sending: 'wysyłanie...',
+
+  // reply
+  mod_badge: 'MOD',
+  content_is_required: 'Treść jest wymagana',
+  nickname_is_required: 'Ksywka jest wymagana',
+
+  comment_has_been_sent:
+    'Twój komentarz został wysłany. Poczekaj na zatwierdzenie.',
+}
