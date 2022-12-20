039cd0666ac0cfee843bc10827a38cd67fe19147
HiveBeats <38073817+HiveBeats@users.noreply.github.com>
2022-12-20 17:07:27 +0400

    refactor: better status messages

--- pages/api/healthcheck.ts
+++ pages/api/healthcheck.ts
@@ -2,13 +2,20 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { HealthCheckService } from "../../service/healthcheck.service";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    if (req.method !== 'GET')
+        return res.status(405).json({ status: 'Method Not Allowed' });
+
     const healthCheckService = new HealthCheckService(req);
-    if (req.method === 'GET') {
+
+    try {
         const result = await healthCheckService.check();
         if (result) {
             res.status(200).json({ status: 'Ok' })
         } else {
             res.status(500).json({ status: 'Error' })
         }
+    }
+    catch (err) {
+        res.status(500).json({ status: 'Error:' + err.message })
     }
 }
--- service/healthcheck.service.ts
+++ service/healthcheck.service.ts
@@ -3,12 +3,12 @@ import { prisma } from '../utils.server'
 
 export class HealthCheckService extends RequestScopeService {
     async check(): Promise<boolean> {
-        try{
+        try {
             const dbConnection = await prisma.$queryRaw`SELECT 1`;
             return dbConnection[0]['1'] == 1;
         }
-        catch(err) {
-            return false;
+        catch (err) {
+            throw err;
         }
     }
 }
