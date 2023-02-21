de72eae26193905d525e7b94719df1c1e9d0ab29
Randy <randypriv@gmail.com>
2023-02-21 20:24:31 +0800

    project update api

---
+++ pages/api/v2/projects/[projectId].ts
@@ -0,0 +1,41 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { prisma } from '../../../../app/utils/prisma'
+import { AuthService } from '../../../../service/auth.service'
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const projectId = req.query.projectId as string
+  const authService = new AuthService(req, res)
+
+  if (req.method === 'PUT') {
+    const body = req.body as {
+      webhookUrl?: string
+      title?: string,
+    }
+
+    const project = await prisma.project.findUnique({
+      where: {
+        id: projectId
+      },
+      select: {
+        ownerId: true
+      }
+    })
+    if (!await authService.projectOwnerGuard(project)) {
+      return
+    }
+
+    await prisma.project.update({
+      where: {
+        id: projectId
+      },
+      data: {
+        // TODO: validate
+        webhook: body.webhookUrl,
+        title: body.title
+      }
+    })
+    res.json({
+      message: 'success'
+    })
+  }
+}
