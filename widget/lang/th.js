c82f8ab6b73000fa4a53713348de6e5c918e4c4b
linnil <39083566+linnilsupak@users.noreply.github.com>
2023-04-04 18:01:47 +0700

    add thai language

---
+++ widget/lang/th.js
@@ -0,0 +1,20 @@
+window.CUSDIS_LOCALE = {
+  powered_by: 'กล่องความเห็นนี้ สนับสนุนโดย Cusdis',
+  post_comment: 'ส่งความเห็น',
+  loading: 'กำลังโหลด',
+
+  // comment
+  email: 'อีเมล (ไม่จำเป็น)',
+  nickname: 'ชื่อเล่น',
+  reply_placeholder: 'ตอบกลับ...',
+  reply_btn: 'ตอบกลับ',
+  sending: 'ส่ง...',
+
+  // reply
+  mod_badge: 'MOD',
+  content_is_required: 'จำเป็นต้องกรอกเนื้อหา',
+  nickname_is_required: 'จำเป็นต้องกรอกชื่อเล่น',
+
+  comment_has_been_sent:
+    'ส่งความเห็นของคุณแล้ว กำลังรอการอนุมัติ',
+}
