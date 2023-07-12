3ab689c91f05ea8f58cd23fab818dd3b8d2fabee
Randy <randypriv@gmail.com>
2023-07-12 17:45:33 +0800

    approve page

--- service/webhook.service.ts
+++ service/webhook.service.ts
@@ -33,6 +33,11 @@ export class WebhookService extends RequestScopeService {
       },
     })
 
+    const approveToken = await this.tokenService.genApproveToken(comment.id)
+    const approveLink = `${resolvedConfig.host}/open/approve?token=${approveToken}`
+
+    console.log(approveLink)
+
     if (project.enableWebhook && !comment.moderatorId && project.webhook) {
 
       const fullComment = await prisma.comment.findUnique({
@@ -53,9 +58,6 @@ export class WebhookService extends RequestScopeService {
           },
         },
       })
-
-      const approveToken = await this.tokenService.genApproveToken(comment.id)
-      const approveLink = `${resolvedConfig.host}/open/approve?token=${approveToken}`
 
       statService.capture('webhook_trigger', {
         properties: {
