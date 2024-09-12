6f06778bea696d9d904b0e08c9e9cfc4ebe526f1
AmirHossein <marjani.amir@gmail.com>
2024-09-12 22:50:26 +0330

    Create fa.js

---
+++ widget/lang/fa.js
@@ -0,0 +1,20 @@
+export default {
+  powered_by: 'نظرات نیرو گرفته از Cusdis',
+  post_comment: 'نظر',
+  loading: 'در حال بارگزاری',
+
+  // comment
+  email: 'ایمیل (اختیاری)',
+  nickname: 'نام مستعار',
+  reply_placeholder: 'پاسخ ...',
+  reply_btn: 'پاسخ',
+  sending: 'درحال ارسال...',
+
+  // reply
+  mod_badge: 'MOD',
+  content_is_required: 'متن الزامی است',
+  nickname_is_required: 'نام مستعار الزامی است',
+
+  comment_has_been_sent:
+    'نظر شما ارسال شد. لطفا منتظر تایید باشید.',
+}
