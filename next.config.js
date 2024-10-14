b1ab80a5a320bec4bf2c56435a12a849469acf40
Randy <randypriv@gmail.com>
2024-10-14 18:14:19 +0800

    add cors for public js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 module.exports = {
+  headers() {
+    return [
+      {
+        source: "/js/*",
+        headers: [
+          { key: "Access-Control-Allow-Origin", value: "*" },
+        ]
+      }
+    ]
+  },
   rewrites() {
     return [
       {
