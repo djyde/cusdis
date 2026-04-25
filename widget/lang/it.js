fce8c2c5e998ac9a5c8d55ded480916cab222d76
Alberto <alberto-bottarini@users.noreply.github.com>
2026-04-25 14:57:18 +0200

    add it

---
+++ widget/lang/it.js
@@ -0,0 +1,20 @@
+export default {          
+    powered_by: 'Commenti gestiti da Cusdis',
+    post_comment: 'Invia commento',                                                                                                                                        
+    loading: 'Caricamento',
+                                                                                                                                                                           
+    // comment         
+    email: 'Email (opzionale)',
+    nickname: 'Nome',                                                                                                                                                      
+    reply_placeholder: 'Scrivi un commento...',
+    reply_btn: 'Rispondi',                                                                                                                                                 
+    sending: 'Invio in corso...',
+
+    // reply
+    mod_badge: 'MOD',
+    content_is_required: 'Il contenuto è obbligatorio',                                                                                                                    
+    nickname_is_required: 'Il nome è obbligatorio',
+                                                                                                                                                                           
+    comment_has_been_sent:
+      'Il tuo commento è stato inviato. Attendi l\'approvazione.',
+} 
